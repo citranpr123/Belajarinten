99b3391ef85be89a3755b8b6762f505523f97f70
citranpr123 <citranopariana@gmail.com>
2023-03-14 17:08:24 +0800

    Initial commit

---
+++ app/src/main/java/com/citra/belajarinten/PindahActivity2.kt
@@ -0,0 +1,11 @@
+package com.citra.belajarinten
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PindahActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah2)
+    }
+}
